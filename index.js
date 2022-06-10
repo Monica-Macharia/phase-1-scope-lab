895f386c9eed3f28b2a9f107ba4d91f0863b5f0d
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-10 05:34:49 +0300

    commit tree 733083395a159b8df0f3b06845e364c1230db079

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer = "peter"
+
+function upperCaseCustomerName (){
+    customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    return window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    return window.bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "sabina"
+}
+
+
